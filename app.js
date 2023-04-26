59f8c1f414ee7e5189322739441a6050b8a462d0
Fuat Tarık İslam <fuattarik.islam@neareasttech.local>
2023-04-26 11:53:18 +0300

    files are upgraded

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-$("#test").html("tarik")
+const ul = document.getElementById('list');
+const input = document.getElementById('task');
 
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
+
+const liMaker = (text) => {
+    const li = document.createElement('li');
+    li.textContent = text;
+    ul.appendChild(li);
+}
+
+function newElement(){
+    itemsArray.push(input.value);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    liMaker(input.value);
+    input.value = "";
+}
