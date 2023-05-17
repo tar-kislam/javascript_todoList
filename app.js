240dcb16e71503122daa5594d99e51856cd9800e
Fuat Tarık İslam <fuattarik.islam@neareasttech.local>
2023-05-17 14:17:07 +0300

    files are upgreaded

--- app.js
+++ app.js
@@ -10,11 +10,48 @@ const liMaker = (text) => {
     const li = document.createElement('li');
     li.textContent = text;
     ul.appendChild(li);
-}
 
-function newElement(){
-    itemsArray.push(input.value);
-    localStorage.setItem('items', JSON.stringify(itemsArray));
-    liMaker(input.value);
-    input.value = "";
+    const span = document.createElement('span');
+    span.className = 'text-end float-end';
+    span.innerHTML = "<i class='fa-solid fa-x'></i>";
+    li.appendChild(span);
+
+    li.addEventListener('click', () => {
+        li.classList.toggle('completed');
+    });
+
+    const icon = span.querySelector('.fa-solid.fa-x');
+    icon.addEventListener('click', (event) => {
+        event.stopPropagation();
+        ul.removeChild(li);
+
+        const index = itemsArray.indexOf(text);
+        if (index !== -1) {
+            itemsArray.splice(index, 1);
+            localStorage.setItem('items', JSON.stringify(itemsArray));
+        }
+    });
+};
+
+// Display the items from localStorage on page load
+data.forEach((item) => {
+    liMaker(item);
+});
+
+
+function newElement() {
+    var toast = new bootstrap.Toast(document.getElementById('toast'));
+    const toastNull = document.getElementById("toastBody");
+    
+    if (input.value.trim() === "") {
+        toastNull.textContent = "Bu alanı boş bırakamazsın!"
+        toast.show();
+    } else {
+        toastNull.textContent = "Listeye Eklendi!"
+        toast.show();
+        itemsArray.push(input.value);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        liMaker(input.value);
+        input.value = "";
+    }
 }
